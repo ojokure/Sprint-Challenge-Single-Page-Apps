c39b7060655a09d48af75614b92582d67424455f
oladimeji-ojo <ojokuredim@gmail.com>
2019-09-20 12:29:37 +0100

    added a search by name feature

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as yup from "yup";
+import Axios from "axios";
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
+  const [search, setSearch] = useState("");
 
-      <Formik
-      initialValues
-      validationSchema
-      onSubmit
-      render={props => {
-        return (
-          <Form>
-            <div>
-              <label> </label>
-              <Field name="name" type="text" placeholder="Search by name" />
-              <ErrorMessage name="name" component="div" />
-            </div>
-            </Form>
-        )}
-        }
-    />
-    </section>)
+  const [result, setResults] = useState([]);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  // useEffect(() => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    Axios.get(`https://rickandmortyapi.com/api/character/?name=${search}`).then(
+      res => {
+        setResults(res.data.results);
+        console.log(res.data.results);
+      }
+    );
+  };
+  // }, [search]);
+
+  return (
+    <>
+      <section className="search-form">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name"> search by name </label>
+            <input
+              value={search}
+              onChange={handleChange}
+              id="name"
+              type="text"
+            />
+          </div>
+        </form>
+      </section>
+      <section className="character-list">
+        {result.map(el => (
+          <div>
+            Name: {el.name} Species:{el.species}
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
