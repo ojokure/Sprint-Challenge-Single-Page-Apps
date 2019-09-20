a73dc7b88abd15d8f352184cec1b320cce5867ee
oladimeji-ojo <ojokuredim@gmail.com>
2019-09-20 08:31:50 +0100

    added usestate to track data from useEffect

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+      const [character, setCharacter] = useState([]);
+
+
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+     .then(res => {
+           setCharacter(res.data.results) 
+     })
+     .catch(err => {
+
+     });
+
+
   }, []);
 
   return (
